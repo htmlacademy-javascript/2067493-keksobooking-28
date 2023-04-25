0d9760a727f1f0d6210f5eff8a07547069909b41
Никита Межецкий <nsm020598@gmail.com>
2023-04-25 19:50:52 +1000

    Добавит функцию для задания случайного числа

--- js/main.js
+++ js/main.js
@@ -26,3 +26,10 @@ const links = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg'
 ];//Ссылки на фотографии
+
+function getRandomInteger(a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+}
+
