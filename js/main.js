6558d0f05e461c15884994733785cb69d7916049
Никита Межецкий <nsm020598@gmail.com>
2023-04-25 19:44:00 +1000

    Создаст необходимые переменные

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Первое задание (14.5) Больше деталей.
 
-/*Созадать массив похожих объявлений.
+/*Создать массив похожих объявлений.
   Массив состоит из объектов:
   1) Объект aurhor (Автор объявление) данный массив состоит из одной строки в которой указываеться адрес изображения
   2) Объект offer (Информация об объявление) состоит из следующих полей:
@@ -10,9 +10,19 @@
         2.4) type (Тип сооружения) - одно из пяти значений (palace (дворец), flat(квартира), house(дом), bungalow(бунгало), hotel(отель));
         2.5) checkin  - одно из из трех фиксированных значение (12:00, 13:00, 14:00);
         2.6) checkout  - одно из из трех фиксированных значение (12:00, 13:00, 14:00)
-        2.7) feature (Особенности) - масиив строк случайной длинны (значения: wifi, dishwasher, parking, washer, elevator, conditione);
+        2.7) features (Особенности) - масиив строк случайной длинны (значения: wifi, dishwasher, parking, washer, elevator, conditione);
         2.8) photos (Фотографии) - массив строк случайной длины (значения: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg
                                                                           https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg
                                                                           https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg)
   3) location (местоположение) - в виде географических координат (lat - широта от 35.65000 до 35.70000) (lng - долгота от 139.70000 до 139.80000)
 */
+
+const MAX_ELEMNTS = 10; // переменная характерезующая маскимальную длинну массива
+const typeBuildings = ['palace', 'flat', 'house', 'bungalow', 'hotel'];// типы зданий
+const times = ['12:00', '13:00', '14:00'];//Время посещения и проверок
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditione'];
+const links = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg'
+];//Ссылки на фотографии
