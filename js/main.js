5631998b2294b459e95d616dd61bf4a3d8059485
Никита Межецкий <nsm020598@gmail.com>
2023-05-02 12:28:27 +1000

    Добавит обьявление на страницу

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import {adForm, filtersForm} from './constains.js';
+import { renderAdvertisment, container } from './render-Advertisment.js';
+import { getData } from './api.js';
+import {getInactiveState} from './inactive-state.js';
+
+getInactiveState(adForm);
+
+
+getData()
+  .then((data) => {
+    renderAdvertisment(data[3]);
+    container.append(renderAdvertisment(data[3]));
+  })
+  .catch();
+
